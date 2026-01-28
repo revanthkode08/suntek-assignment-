779567fb803cec3cc50ede15c33f052945539a3b
revanthkode08 <revanthkode08@gmail.com>
2026-01-28 07:34:18 +0530

    Add files via upload

---
+++ MERNSTACK-assignment/MERNSTACK-main/Week-1/ob.js
@@ -0,0 +1,10 @@
+let a=10;
+let username="ravi"
+let status=true
+let marks=[50,60,70]
+let student={id:1,name:"chethan"}
+console.log(typeof a )
+console.log(typeof status)
+console.log(typeof username)
+console.log(typeof marks)
+console.log(typeof student)
---
+++ MERNSTACK-assignment/MERNSTACK-main/Week-1/operators/task1.js
@@ -0,0 +1,10 @@
+let totalAmount=0;
+totalAmount+=500;
+console.log(totalAmount)
+totalAmount+=1200
+console.log(totalAmount)
+totalAmount-=200
+console.log(totalAmount)
+let gst=totalAmount * 0.18
+totalAmount+=gst;
+console.log(totalAmount)
