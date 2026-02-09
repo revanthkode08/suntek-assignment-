46cf1f4790a39c91c3636d2e23835e4cc23a01a2
revanthkode08 <revanthkode08@gmail.com>
2026-02-09 23:49:28 +0530

    Delete suntekpractices directory

--- suntekpractices/1.js
+++
@@ -1,5 +0,0 @@
-console.log("hello world")
-let a=10
-console.log(typeof a)
-let username='ram'
-console.log(typeof username)
--- suntekpractices/14.js
+++
@@ -1,6 +0,0 @@
- //modification method
- let salary=[100,200,300]
- let updatesalary=salary.map(function(salary){
-    return salary+50
- })
- console.log(updatesalary)
